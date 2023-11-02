bc7fe4168cc1d35ba857984823e8731e1d26bb78
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-11-02 08:53:59 +0000

    join as update

--- src/components/JoinAs/JoinAs.js
+++ src/components/JoinAs/JoinAs.js
@@ -1,4 +1,7 @@
 import "./join.css";
+import djside from "../../assets/djCover.jpg";
+import artistside from "../../assets/djSide/images/music photos/music1.jpg";
+
 
 const JoinAs = () => {
     return(
@@ -6,7 +9,7 @@ const JoinAs = () => {
        <section className="hero-section">
       <div className="card-grid">
         <a className="card" href="/signup">
-          <div className="card__background" style={{ backgroundImage: `url('./src/media/covers/modern-microphone.jpg')` }}></div>
+        <div className="card__background" style={{ backgroundImage: `url(${artistside})` }}></div>
           <div className="card__content">
             <p className="card__category">Join us as an:</p>
             <h3 className="card__heading">Artist</h3>
@@ -14,7 +17,7 @@ const JoinAs = () => {
         </a>
 
         <a className="card" target="_blank" href="https://droplix-first.netlify.app/" rel="noreferrer">
-  <div className="card__background" style={{ backgroundImage: `url('./src/media/djCover.jpg')` }}></div>
+        <div className="card__background" style={{ backgroundImage: `url(${djside})` }}></div>
   <div className="card__content">
     <p className="card__category">Get Access as a:</p>
     <h3 className="card__heading">DJ</h3>
